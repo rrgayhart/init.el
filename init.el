054b8b4aea46b9be67421fdb69acdbd75b58db35
Meeka <rrgayhart@gmail.com>
2014-01-30 21:45:38 -0700

    ruby and rails setup with linenumbering and dash and point

---
+++ init.el
@@ -0,0 +1,61 @@
+(require 'package)
+(add-to-list 'package-archives
+       '("melpa". "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+(global-set-key (kbd "S-<left>") 'windmove-left)
+(global-set-key (kbd "S-<right>") 'windmove-right)
+(global-set-key (kbd "S-<up>") 'windmove-up)
+(global-set-key (kbd "S-<down>") 'windmove-down)
+
+(global-set-key "\C-w" 'backward-kill-word)
+
+(global-set-key (kbd "M-p") 'projectile-find-file)
+(global-set-key "\C-cd" 'dash-at-point)
+(global-linum-mode t)
+
+
+(projectile-global-mode)
+
+(load-theme 'adwaita t)
+(blink-cursor-mode t)
+(set-face-attribute 'default nil :height 140)
+
+(setq coffee-tab-width 2)
+
+(setq enh-ruby-program "/Users/rrgayhart/.rvm/rubies/ruby-2.0.0-p247/bin/ruby")
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+ 
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+ 
+(setq enh-ruby-bounce-deep-indent t)
+(setq enh-ruby-hanging-brace-indent-level 2)
+ 
+(require 'cl) ; If you don't have it already
+ 
+(defun* get-closest-gemfile-root (&optional (file "Gemfile"))
+  "Determine the pathname of the first instance of FILE starting from the current directory towards root.
+This may not do the correct thing in presence of links. If it does not find FILE, then it shall return the name
+of FILE in the current directory, suitable for creation"
+  (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
+    (loop 
+     for d = default-directory then (expand-file-name ".." d)
+     if (file-exists-p (expand-file-name file d))
+     return d
+     if (equal d root)
+     return nil)))
+ 
+(require 'compile)
